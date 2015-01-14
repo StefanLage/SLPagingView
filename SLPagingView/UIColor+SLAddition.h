afc6159e891b9c4097cfa48b7ad8f7cdc13fde08
Jack <xhzengAIB@gmail.com>
2015-01-14 14:13:52 +0800

    1.fix the TinderStoryboard example run error  2.Add UIColor addition

---
+++ SLPagingView/UIColor+SLAddition.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+SLAddition.h
+//  TinderLike
+//
+//  Created by kingsoft_ios on 15/1/14.
+//  Copyright (c) 2015年 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SLAddition)
+
++ (UIColor *)gradient:(double)percent top:(double)topX bottom:(double)bottomX init:(UIColor*)init goal:(UIColor*)goal;
+
+@end
