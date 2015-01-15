83e3b6142c50ef677c63430f063980c5253fadb6
Stefan Lage <lagestfan@gmail.com>
2015-01-15 01:31:29 +0100

    Support landscape mode
    All subviews are adapted to the landscape mode and also during the
    rotation

--- Demos/TinderStoryboard/TinderStoryboard/AppDelegate.m
+++ Demos/TinderStoryboard/TinderStoryboard/AppDelegate.m
@@ -20,7 +20,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"%@", self.window.rootViewController.childViewControllers.firstObject);
     UIColor *orange = [UIColor colorWithRed:255/255
                                       green:69.0/255
                                        blue:0.0/255
@@ -31,32 +30,36 @@
                                      blue:.84
                                     alpha:1.0];
     
-    SLPagingViewController *pageViewController = self.window.rootViewController.childViewControllers.firstObject;
-    [pageViewController setCurrentIndex:1 animated:NO];
+    SLPagingViewController *pageViewController  = self.window.rootViewController.childViewControllers.firstObject;
+    pageViewController.navigationSideItemsStyle = SLNavigationSideItemsStyleOnBounds;
+    [pageViewController setCurrentIndex:1
+                               animated:NO];
+    float minX                                  = 45.0;
+    // Tinder Like
     pageViewController.pagingViewMoving = ^(NSArray *subviews){
-        int i = 0;
+        float mid  = [UIScreen mainScreen].bounds.size.width/2 - minX;
+        float midM = [UIScreen mainScreen].bounds.size.width - minX;
         for(UIImageView *v in subviews){
             UIColor *c = gray;
-            if(v.frame.origin.x > 45
-               && v.frame.origin.x < 145)
+            if(v.frame.origin.x > minX
+               && v.frame.origin.x < mid)
                 // Left part
                 c = [UIColor gradient:v.frame.origin.x
-                                  top:46
-                               bottom:144
+                                  top:minX+1
+                               bottom:mid-1
                                  init:orange
                                  goal:gray];
-            else if(v.frame.origin.x > 145
-                    && v.frame.origin.x < 245)
+            else if(v.frame.origin.x > mid
+                    && v.frame.origin.x < midM)
                 // Right part
                 c = [UIColor gradient:v.frame.origin.x
-                                  top:146
-                               bottom:244
+                                  top:mid+1
+                               bottom:midM-1
                                  init:gray
                                  goal:orange];
-            else if(v.frame.origin.x == 145)
+            else if(v.frame.origin.x == mid)
                 c = orange;
             v.tintColor= c;
-            i++;
         }
     };
     
